7f40fca542eedcdd07113e7903d366e6908c5d0a
Mohammad-Mahdi1999 <44753085+Mohammad-Mahdi1999@users.noreply.github.com>
2018-11-07 15:45:17 +0330

    Start of Sum!

---
+++ Sum/Sum.cpp
@@ -0,0 +1,18 @@
+// In the name of God !
+//Sum
+//V1 
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int num1, num2, sum;
+	cout << "Enter two numbers :" << endl;
+	cin >> num1;
+	cin >> num2;
+	sum = num1 + num2;
+	cout << "Sum of the numbers  is " << sum << "!" << endl;
+	system("pause");
+}
+//Created by /%++;; Mohammad-Mahdi Farah-bakhsh ;;++%\
